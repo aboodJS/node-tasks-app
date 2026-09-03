d55bfccbf730cad009272406b0b18bb24f35ce6d
ajs <140010746+aboodJS@users.noreply.github.com>
2026-09-03 17:49:16 +0300

    added more functions

--- index.js
+++ index.js
@@ -17,10 +17,58 @@ async function addTask() {
   });
 }
 
+async function ListTasks(type) {
+  let data = JSON.parse(fs.readFileSync("tasks.json", "utf8"));
+  if (type === undefined) {
+    data.forEach((element) => {
+      console.log(
+        `${element.id}- task: ${element.title}, ${element.done === true ? "done" : "to-do"}`,
+      );
+    });
+  } else if (type === "complete") {
+    data.forEach((element) => {
+      if (element.done === true) {
+        console.log(
+          `${element.id}- task: ${element.title}, ${element.done === true ? "done" : "to-do"}`,
+        );
+      }
+    });
+  } else if (type === "incomplete") {
+    data.forEach((element) => {
+      if (element.done === false) {
+        console.log(
+          `${element.id}- task: ${element.title}, ${element.done === true ? "done" : "to-do"}`,
+        );
+      }
+    });
+  }
+}
+
+async function DeleteTask(taskId) {
+  let data = JSON.parse(fs.readFileSync("tasks.json", "utf8"));
+  let filteredData = data.filter((e) => e.id !== taskId);
+  await fs.writeFile(
+    "tasks.json",
+    `${JSON.stringify(filteredData)}`,
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    },
+  );
+}
+
 if (args[2] === "help") {
   console.log(`Usage: help: prints this message ,add "title": adds a task`);
 } else if (args[2] === "add") {
   addTask();
+} else if (args[2] === "list") {
+  ListTasks(args[3]);
+} else if (args[2] === "delete") {
+  DeleteTask(Number.parseInt(args[3]));
 } else {
-  console.log(`Usage: help: prints this message ,add "title": adds a task`);
+  console.log(
+    `Usage: help: prints this message | add "title": adds a task | list "complete | incomplete": lists tasks | delete "id": deletes a task`,
+  );
 }
